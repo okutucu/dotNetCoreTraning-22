3c16a96faab8b7743d3129dd94f8279f9d46c622
Oğuzhan <58344612+okutucu@users.noreply.github.com>
2022-03-18 22:21:55 +0300

    Foreach-While

--- For_Loop_Break_Contiune/For_Loop_Break_Contiune/Program.cs
+++ For_Loop_Break_Contiune/For_Loop_Break_Contiune/Program.cs
@@ -10,56 +10,80 @@ namespace For_Loop_Break_Contiune
     {
         static void Main(string[] args)
         {
-            Console.Write("Lütfen bir sayı giriniz : ");
-            int sayac = int.Parse(Console.ReadLine());
+            //Console.Write("Lütfen bir sayı giriniz : ");
+            //int sayac = int.Parse(Console.ReadLine());
 
-            for (int i = 0; i <= sayac; i++)
+            //for (int i = 0; i <= sayac; i++)
+            //{
+            //    if (i%2 ==1)
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+
+            //}
+
+            //int tekToplam = 0;
+            //int ciftToplam = 0;
+
+            //for (int i = 0; i <= 1000; i++)
+            //{
+            //    if (i % 2 == 1)
+            //    {
+            //        tekToplam += i;
+
+            //    }
+            //    else
+            //    {
+            //        ciftToplam += i;
+            //    }
+            //}
+
+            //Console.WriteLine("Tek Toplam : "  + tekToplam);
+            //Console.WriteLine("Çift Toplam : "  + ciftToplam);
+
+
+            //// Break - Contiune
+
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    if (i==4)
+            //    {
+            //        break;
+            //    }
+            //    Console.WriteLine(i);
+            //}
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    if (i == 4)
+            //    {
+            //        continue;
+            //    }
+            //    Console.WriteLine(i);
+            //}
+
+            Console.Write("SAyı : ");
+            int n = int.Parse(Console.ReadLine());
+
+            string result = "";
+            int x = n;
+            for (int i = 0; i < n; i++)
             {
-                if (i%2 ==1)
+                
+                x--;
+
+                for (int j = 0; j < x; j++)
                 {
-                    Console.WriteLine(i);
+                    Console.Write(" ");
                 }
+
+                result += "#";
+
+                Console.WriteLine(result);
 
             }
 
-            int tekToplam = 0;
-            int ciftToplam = 0;
+            Console.ReadLine();
 
-            for (int i = 0; i <= 1000; i++)
-            {
-                if (i % 2 == 1)
-                {
-                    tekToplam += i;
-                    
-                }
-                else
-                {
-                    ciftToplam += i;
-                }
-            }
-
-            Console.WriteLine("Tek Toplam : "  + tekToplam);
-            Console.WriteLine("Çift Toplam : "  + ciftToplam);
-
-
-            // Break - Contiune
-
-            for (int i = 0; i < 10; i++)
-            {
-                if (i==4)
-                {
-                    break;
-                }
-                Console.WriteLine(i);
-            }
-            for (int i = 0; i < 10; i++)
-            {
-                if (i == 4)
-                {
-                    continue;
-                }
-                Console.WriteLine(i);
-            }
 
 
         }
---
+++ While-Foreach/While-Foreach/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace While_Foreach
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int sayi = int.Parse(Console.ReadLine());
+            int sayac = 1;
+            int toplam = 0;
+
+            while (sayac <= sayi)
+            {
+                toplam += sayac;
+                sayac++;
+            }
+
+            Console.WriteLine(toplam/sayi);
+
+            char character = 'a';
+            while (character < 'z')
+            {
+                Console.WriteLine(character);
+                character++;
+            }
+            string[] arabalar = { "BMW", "FORD", "TOYOTA", "NİSSAN" };
+             
+            foreach (var item in arabalar)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+
+
+        
+    }
+}
