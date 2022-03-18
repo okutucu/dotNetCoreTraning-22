4de125197b32a992383b283881f50e145235dc5c
Oğuzhan <58344612+okutucu@users.noreply.github.com>
2022-03-18 22:31:43 +0300

    Array-1

---
+++ Array/Array/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] renkler = new string[5];
+
+            string[] hayvanlar = { "Kedi", "Köpek", "Kuş", "Maymun" };
+
+            int[] dizi;
+            dizi = new int[5];
+
+            renkler[0] = "Mavi";
+            dizi[3] = 10;
+            Console.WriteLine(hayvanlar[1]);
+            Console.WriteLine(dizi[3]);
+            Console.WriteLine(renkler[0]);
+
+            Console.WriteLine("Lütfen dizinin eleman sayısını giriniz :");
+            int diziUzunlugu = int.Parse(Console.ReadLine());
+            int[] sayiDizisi = new int[diziUzunlugu];
+
+            for (int i = 0; i < diziUzunlugu; i++)
+            {
+                Console.WriteLine("Lütfen {0}. sayısı giriniz : ", i+1);
+                sayiDizisi[i] = int.Parse(Console.ReadLine());
+            }
+
+            int toplam = 0;
+            foreach (var item in sayiDizisi)
+            {
+                toplam += item;
+            }
+
+            Console.WriteLine("Ortlama : " + toplam/diziUzunlugu);
+        }
+    }
+}
