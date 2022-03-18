08b4bc8ab2112304db31fc111afb46e25e424c80
Oğuzhan <58344612+okutucu@users.noreply.github.com>
2022-03-18 23:59:42 +0300

    Array-2

--- Array/Array/Program.cs
+++ Array/Array/Program.cs
@@ -10,36 +10,69 @@ namespace Array
     {
         static void Main(string[] args)
         {
-            string[] renkler = new string[5];
+            //string[] renkler = new string[5];
 
-            string[] hayvanlar = { "Kedi", "Köpek", "Kuş", "Maymun" };
+            //string[] hayvanlar = { "Kedi", "Köpek", "Kuş", "Maymun" };
 
-            int[] dizi;
-            dizi = new int[5];
+            //int[] dizi;
+            //dizi = new int[5];
 
-            renkler[0] = "Mavi";
-            dizi[3] = 10;
-            Console.WriteLine(hayvanlar[1]);
-            Console.WriteLine(dizi[3]);
-            Console.WriteLine(renkler[0]);
+            //renkler[0] = "Mavi";
+            //dizi[3] = 10;
+            //Console.WriteLine(hayvanlar[1]);
+            //Console.WriteLine(dizi[3]);
+            //Console.WriteLine(renkler[0]);
 
-            Console.WriteLine("Lütfen dizinin eleman sayısını giriniz :");
-            int diziUzunlugu = int.Parse(Console.ReadLine());
-            int[] sayiDizisi = new int[diziUzunlugu];
+            //Console.WriteLine("Lütfen dizinin eleman sayısını giriniz :");
+            //int diziUzunlugu = int.Parse(Console.ReadLine());
+            //int[] sayiDizisi = new int[diziUzunlugu];
 
-            for (int i = 0; i < diziUzunlugu; i++)
+            //for (int i = 0; i < diziUzunlugu; i++)
+            //{
+            //    Console.WriteLine("Lütfen {0}. sayısı giriniz : ", i+1);
+            //    sayiDizisi[i] = int.Parse(Console.ReadLine());
+            //}
+
+            //int toplam = 0;
+            //foreach (var item in sayiDizisi)
+            //{
+            //    toplam += item;
+            //}
+
+            //Console.WriteLine("Ortlama : " + toplam/diziUzunlugu);
+
+
+            int[] sayilar ={ -4, -3, -9, 0, 4, 1 };
+
+            int arrayLength = sayilar.Length;
+            decimal countP = 0;
+            decimal countN = 0;
+            decimal countZ = 0;
+
+            foreach (int i in sayilar)
             {
-                Console.WriteLine("Lütfen {0}. sayısı giriniz : ", i+1);
-                sayiDizisi[i] = int.Parse(Console.ReadLine());
+                if (i==0)
+                {
+                    countZ++;
+                }
+                else if (i<0)
+                {
+                    countN++;
+                }
+                else
+                {
+                    countP++;
+                }
             }
 
-            int toplam = 0;
-            foreach (var item in sayiDizisi)
-            {
-                toplam += item;
-            }
+            Console.WriteLine(Math.Round((countP/arrayLength),6));
+            Console.WriteLine(Math.Round((countN/arrayLength),6));
+            Console.WriteLine(Math.Round((countZ/arrayLength),6));
+           
 
-            Console.WriteLine("Ortlama : " + toplam/diziUzunlugu);
+
+
+            Console.ReadLine();
         }
     }
 }
---
+++ Array_2/Array_2/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] sayiDizisi = { 23, 12, 4, 86, 72, 3, 11, 17 };
+
+            foreach (var item in sayiDizisi)
+            {
+                Console.WriteLine(item);
+            }
+
+
+            Array.Sort(sayiDizisi);
+
+            foreach (var item in sayiDizisi)
+            {
+                Console.WriteLine(item);
+            }
+
+            Array.Clear(sayiDizisi, 2, 2);
+
+            foreach (var item in sayiDizisi)
+            {
+                Console.WriteLine(item);
+            }
+
+
+            Array.Reverse(sayiDizisi);
+
+            foreach (var item in sayiDizisi)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine(Array.IndexOf(sayiDizisi,23));
+
+            Array.Resize<int>(ref sayiDizisi, 9);
+        }
+    }
+}
