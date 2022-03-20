74e105c0c5e19604814d15a9d79208f5bba426a7
Oğuzhan <58344612+okutucu@users.noreply.github.com>
2022-03-20 06:46:50 +0300

    Project_Completed

---
+++ Static_Sinif_ve_Uyeler/Static_Sinif_ve_Uyeler/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Static_Sinif_ve_Uyeler
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Çalışan Sayısı : {0}",Calisan.CalisanSayisi);
+
+
+
+            Calisan calisan = new Calisan("Ayşe","Yılmaz","IK");
+            Console.WriteLine("Çalışan Sayısı : {0}", Calisan.CalisanSayisi);
+
+            Calisan calisan1 = new Calisan("Deniz", "Arda", "IK");
+            Calisan calisan2 = new Calisan("Zikriye", "Ürkmez", "IK");
+
+            Console.WriteLine("Çalışan Sayısı : {0}", Calisan.CalisanSayisi);
+
+
+            Console.WriteLine("Toplama İşlemi Sonucu {0}", Islemler.Topla(100, 200));
+            Console.WriteLine("Çıkarma İşlemi Sonucu {0}", Islemler.Cikar(100, 200));
+
+
+
+        }
+    }
+
+    class Calisan
+    {
+        private static int calisanSayisi;
+
+        public static int CalisanSayisi { get => calisanSayisi;}
+
+        private string Isim;
+
+        private string Soyisim;
+
+        private string Departman;
+
+        static Calisan()
+        {
+            calisanSayisi = 0;
+        }
+        public Calisan(string ısim, string soyisim, string departman)
+        {
+            this.Isim = ısim;
+            this.Soyisim = soyisim;
+            this.Departman = departman;
+            calisanSayisi++;
+        }
+    }
+
+    static class Islemler
+    {
+        public static long Topla(int sayi1,int sayi2)
+        {
+            return sayi1 + sayi2;
+        }
+
+        public static long Cikar(int sayi1, int sayi2)
+        {
+            return sayi1 - sayi2;
+        }
+
+    }
+}
