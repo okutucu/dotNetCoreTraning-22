852fdf048e2a3fa8f3be237a69c60ef00e757d21
Oğuzhan <58344612+okutucu@users.noreply.github.com>
2022-03-20 07:08:39 +0300

    Project_Completed

---
+++ Enum/Enum/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Gunler.Pazar);
+            Console.WriteLine((int)Gunler.Cumartesi);
+
+            int sicaklik = 32;
+
+            if (sicaklik <= (int)HavaDurumu.Normal)
+            {
+                Console.WriteLine("Dışarıya çıkmak için havanın biraz daha ısınmasını bekleyelim");
+            }
+            else if (sicaklik >= (int)HavaDurumu.Sıcak)
+            {
+                Console.WriteLine("Dışarıya çıkmak için çok sıcak bir gün");
+            }
+            else if (sicaklik>=(int)HavaDurumu.Normal && sicaklik < (int)HavaDurumu.CokSıcak)
+            {
+                Console.WriteLine("Hadi dışarı çıkalım...");
+            }
+
+
+        }
+    }
+
+    enum Gunler
+    {
+        Pazartesi =1,
+        Salı,
+        Çarşamba,
+        Perşembe,
+        Cuma,
+        Cumartesi,
+        Pazar
+    }
+    enum HavaDurumu
+    {
+        Soguk = 5,
+        Normal = 20,
+        Sıcak = 25,
+        CokSıcak = 30
+    }
+}
